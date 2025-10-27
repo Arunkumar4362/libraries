b40acfdf379c9076d5f58b9ef7eb154d6225e8e8
Arunkumar4362 <21jr1a4362@gmail.com>
2025-10-27 09:23:27 +0530

    new numpy file created

---
+++ numpy_ex.py
@@ -0,0 +1,12 @@
+import pandas as pd
+file_path_alt = "C:\ArunWorkSpace\librariers\student_exam_scores.csv"
+
+try:
+    df = pd.read_csv(file_path_alt)
+    print("--- File Loaded Successfully (using forward slashes) ---")
+    print(df.head())
+
+except FileNotFoundError:
+    print(f"Error: File not found at path: {file_path_alt}")
+except Exception as e:
+    print(f"An error occurred: {e}")
